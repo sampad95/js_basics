63a7d334bf065ee75417c91f48549a70afc5ac82
sampad mandal   ........................সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-04-27 20:25:45 +0530

    Add files via upload

--- odd_even.js
+++ odd_even.js
@@ -2,18 +2,21 @@ const prompt = require("prompt-sync")()
 var n = prompt("Enter the number: ")
 
 /*
-if(n%2 == 0)
-	console.log(n, "is Even")}
+if(n%2 == 0){
+	console.log(n+ " is Even")}
 else{
-console.log(n, "is Odd")}
+console.log(n+ " is Odd")}
 */
 
-// n%2 == 0 ?
-// 	console.log(n, "is even"):
-// 	console.log(n, "is odd")
+/*
+n%2 == 0 ?
+	console.log(`${n} is even`):
+	console.log(`${n} is odd`)
+*/
+
 
 let result = n%2 == 0?
-	"is even":
-	"is odd"
+	`${n} is even`:
+	`${n} is odd`
 
-console.log(n, result)
+console.log(result)
