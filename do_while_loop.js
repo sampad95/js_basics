2c015c4abb35a980e38e6d4bb492de9dbe834940
sampad mandal   ........................সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-04-27 21:59:57 +0530

    Add files via upload

---
+++ do_while_loop.js
@@ -0,0 +1,18 @@
+/*
+let i = 1
+do{
+    console.log("Hello") // first execute
+    i++                  
+}
+while(i <= 3)            // then check
+console.log(i)
+*/
+
+
+let i = 4
+do{
+    console.log("Hello") // first execute. Atleast one execution whatever be the condition
+    i++                  
+}
+while(i <= 3)            // then check
+console.log(i)
---
+++ while_loop.js
@@ -0,0 +1,25 @@
+/*
+let i = 1
+while(i <= 3){
+    console.log("Hello")
+    i++
+}
+console.log(i) // icrement continues after i=3 to 4 (and that i=4 is printed), but does not print Hello as 4>3.
+*/
+
+/*
+let i = 3
+while(i >= 1){
+    console.log("Hello")
+    i--
+}
+console.log(i) // derement continues after i=1 to 0 (and that i=0 is printed), but does not print Hello as 0<1.
+*/
+
+let i = 4
+while(i <= 3){
+    console.log("Hello")       // No Hello printing
+    i++
+}
+console.log(i)
+
