660664633395523eed5dd0a79d702c1b74b86bcf
sampad mandal   ........................সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-04-27 15:20:01 +0530

    Add files via upload

--- grading_conditional.js
+++ grading_conditional.js
@@ -16,6 +16,7 @@ else{
 	console.log("Don't worry, you just failed in paper. Try next time. Best of luck for the real life exam")}
 */
 
+/*
 marks > 30 && marks <=60?
         console.log("Grade C"):
 marks > 60 && marks <=70?
@@ -27,3 +28,16 @@ marks > 80 && marks <=90?
 marks > 90 && marks <= 100?
         console.log("Grade-AA"):
 console.log("Don't worry, you just failed in paper. Try next time. Best of luck for the real life exam")
+*/
+
+function grading(mks){
+	let result = mks > 30 && mks <=60? "Grade C":
+	mks > 60 && mks <=70? "Grade-B":
+	mks > 70 && mks <= 80? "Grade-A":
+	mks > 80 && mks <=90? "Grade-A+":
+	mks > 90 && mks <= 100? "Grade-AA":
+	"Don't worry, you just failed in paper. Try next time. Best of luck for the real life exam"
+	console.log(result)
+}
+
+grading(marks)
