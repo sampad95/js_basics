6755001a45fcd9e7373c0c28db82ea48e8c32da5
sampad mandal   ........................সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-04-27 15:59:46 +0530

    Add files via upload

--- odd_even.js
+++ odd_even.js
@@ -8,6 +8,12 @@ else{
 console.log(n, "is Odd")}
 */
 
-n%2 == 0 ?
-	console.log(n, "is even"):
-	console.log(n, "is odd")
+// n%2 == 0 ?
+// 	console.log(n, "is even"):
+// 	console.log(n, "is odd")
+
+let result = n%2 == 0?
+	 "is even":
+	 "is odd"
+
+console.log(n, result)
