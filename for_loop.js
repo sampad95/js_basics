82e00485eacbe022ee5bd0362e0b57a157455484
sampad mandal   ........................সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-04-28 13:01:54 +0530

    Add files via upload

---
+++ for_loop.js
@@ -0,0 +1,60 @@
+/*
+for(let i=1; i<=3; i++){
+    console.log("Hello")
+}
+*/
+
+/*
+let i=1
+for(; i<=3; ){
+    console.log("Hello")
+    i++
+}
+*/
+
+/*
+for(let i=1; i<=3; ){
+    console.log("Hello")
+    i++
+}
+*/
+
+/*
+let i=1
+for(; i<=3; i++){
+    console.log("Hello")
+}
+*/
+
+
+// for loop inside for loop
+/*
+for(let i=1; i<=3; i++){
+    console.log("Hello", i)
+    
+    for(let j=1; j<=3; j++){
+        console.log("Kemon acho", j)
+    }
+}
+*/
+
+/*
+for(let i=1; i<=3; i++){
+    console.log("Hello", i)
+    
+    for(let j=1; j<=i; j++){
+        console.log("Kemon acho", j)
+    }
+}
+*/
+
+
+for(let i=1; i<=3; i++){
+    console.log("Hello", i)
+    
+    for(let j=3; j>=i; j--){
+        console.log("Kemon acho", j)
+    }
+}
+
+
