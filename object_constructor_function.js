b0784b06c2aa97b3d70328aee9c235e0fc2d635e
sampad mandal   ........................সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-05-01 00:36:37 +0530

    Add files via upload

--- object_constructor_function.js
+++ object_constructor_function.js
@@ -41,7 +41,7 @@ console.log(person2)
 
 
 // To add a new property to a constructor, you must add it to the constructor function:
-
+/*
 function person(name, age, course, exp){
 
     this.name = name
@@ -58,4 +58,22 @@ const person2 = new person ("Ram", 23, "MSc", 2)
 console.log(person1)
 console.log(person2)
 
+*/
 
+
+
+function person(name, age, course, exp, nationality){
+
+    this.name = name
+    this.age = age
+    this.course = course
+    this.exp = exp
+    this.nationality = nationality
+
+}
+
+const person1 = new person("Sampad", 28, "PhD", 3, "Indian")
+const person2 = new person ("Ram", 23, "MSc", 2, "Indian")
+
+console.log(person1)
+console.log(person2)
