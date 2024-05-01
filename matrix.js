4cd98da562c60ba7f6e99c88df79c7a3fa1e1291
sampad mandal   ........................সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-05-01 22:25:30 +0530

    Add files via upload

--- matrix.js
+++ matrix.js
@@ -1,13 +1,10 @@
 function null_mat(m, n){
 
-    null_matrix = []
-
+    let null_matrix = []
     for(let i = 1; i <= m; i++){
         
-        n_mat = []
-
+        let n_mat = []
         for(let j = 1; j <= n; j++){
-
             n_mat.push(0)
         }
         null_matrix.push(n_mat)
@@ -18,17 +15,13 @@ function null_mat(m, n){
 
 console.log(null_mat(5, 4))
 
-
 function identity_mat(m){
 
-    identity_matrix = []
-
+    let identity_matrix = []
     for(let i = 1; i <= m; i++){
         
-        i_mat = []
-
+        let i_mat = []
         for(let j = 1; j <= m; j++){
-
             if( i===j ){
                 i_mat.push(1)
             } 
@@ -42,3 +35,52 @@ function identity_mat(m){
 }
 
 console.log(identity_mat(5))
+
+
+
+function null_mat2(m, n){
+
+    let null_matrix2 = []
+    let i = 1
+    while(i <= m){
+
+        let n_mat2 = []
+        let j = 1
+        while(j <= n){
+            n_mat2.push(0)
+            j++
+        }
+        i++
+        null_matrix2.push(n_mat2)
+    }
+    return null_matrix2
+}
+
+console.log(null_mat2(5, 4))
+
+
+
+function identity_mat2(m){
+
+    let identity_matrix2 = []
+    let i = 1
+    while(i <= m){
+
+        let i_mat2 = []
+        let j = 1
+        while(j <= m){
+            if(i===j){
+                i_mat2.push(1)
+            }
+            else{
+                i_mat2.push(0)
+            } 
+            j++
+        }
+        i++
+        identity_matrix2.push(i_mat2)
+    }
+    return identity_matrix2
+}
+
+console.log(identity_mat2(5))
