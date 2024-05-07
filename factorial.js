fde19918f943118bfea7bfdbdc187a8af46cccc3
sampad mandal   ........................সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-05-07 08:38:22 +0530

    Add files via upload

--- factorial.js
+++ factorial.js
@@ -1,32 +1,36 @@
 const prompt = require("prompt-sync")()
-//var n = prompt("Enter the number: ")
 
 /*
 function factorial(n){
-    if (n >= 0){
-        let i = 1
-        let f = 1
-        while(i <= n){
+    if (n >= 0 && n % 1 == 0 ){
+        i = 1
+        f = 1
+        while (i <= n){
             f *= i
             i++
-        }    
+        }
         return f
     }
-    else{
-        return "Eneter positive number"}
+    else {
+        return "Enter positive integer number"
+    }
 }
 */
 
 
 function factorial(n){
-    if (n >= 0){
-        if (n==0 || n==1){
-            return 1}
-        else{
-            return n*factorial(n-1)}
+    if (n >= 0 && n % 1 == 0){
+        if (n ==0 || n == 1){
+            return 1
+        }
+        else {
+            return n*factorial(n-1)
+        }
     }
-    else {
-    return "Eneter positive number"}
+    else{
+        return "Enter positive integer number"
+    }
+
 }
 
 
