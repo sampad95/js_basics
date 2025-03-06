ac95b6d64232717a71972e2f09e751d3037eb8eb
sampad mandal     ................................................           সম্পদ মণ্ডল <sampadmandals@gmail.com>
2025-03-06 21:48:48 +0530

    Add files via upload

--- factorial.js
+++ factorial.js
@@ -2,33 +2,33 @@ const prompt = require("prompt-sync")()
 
 /*
 function factorial(n){
-    if (n >= 0 && n % 1 == 0 ){
-        i = 1
-        f = 1
+    if (n >= 0 && n % 1 === 0 ){
+        i = 1;
+        f = 1;
         while (i <= n){
-            f *= i
-            i++
+            f *= i;
+            i++;
         }
         return f
     }
     else {
-        return "Enter positive integer number"
+        return "Enter positive integer number";
     }
 }
 */
 
 
 function factorial(n){
-    if (n >= 0 && n % 1 == 0){
-        if (n ==0 || n == 1){
-            return 1
+    if (n >= 0 && n % 1 === 0){
+        if (n === 0 || n === 1){
+            return 1;
         }
         else {
-            return n*factorial(n-1)
+            return n*factorial(n-1);
         }
     }
     else{
-        return "Enter positive integer number"
+        return "Enter positive integer number";
     }
 
 }
