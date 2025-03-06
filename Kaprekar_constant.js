444172144fb5a367d528c910792036b598b336a3
sampad mandal     ................................................           সম্পদ মণ্ডল <sampadmandals@gmail.com>
2025-03-06 12:05:05 +0530

    Add files via upload

--- Kaprekar_constant.js
+++ Kaprekar_constant.js
@@ -37,9 +37,10 @@ while(n!=6174){
 */
 
 
-// let n = Math.floor(Math.random() * 8999) + 1001;  // Math.floor(Math.random() * (max - min + 1) ) + min; 
-// Here max = 9999, min = 1001
-let n=9990
+/*
+// let n = Math.floor(Math.random() * 8999) + 1001;  // Math.floor(Math.random() * (max - min + 1) ) + min; // Here max = 9999, min = 1001
+var n = Math.floor(Math.random() * 9901) + 100;  // Math.floor(Math.random() * (max - min + 1) ) + min; // Here max = 10000, min = 100
+// let n = 9990;
 console.log("Initial n = ", n);
 
 let i = 0;
@@ -76,7 +77,57 @@ if (n.toString().length === 4){
             console.log("Next n=", n);
         }  
     }
+    console.log("Stop");
 }
 else{
-    console.log("Invalid")
+    console.log("Invalid");
 }
+*/
+
+
+
+function Kaprekar_routine(n){
+    console.log("Initial n = ", n);
+    var i = 0;
+    if (n.toString().length === 4){
+        while(n!=6174){
+            min_n = parseInt(n.toString().split('').sort().join(''));
+            max_n = parseInt(n.toString().split('').sort((a,b)=>b-a).join(''));
+            
+            if (min_n === max_n){
+                console.log("Invalid");
+                break;
+            }
+            else{
+                diff = max_n - min_n;
+                 // i++;
+                 i +=1;
+                 console.log('Step = ', i);
+                 console.log(min_n, max_n, diff);
+    
+                if(diff.toString().length===4){
+                    n=diff;
+                }
+                else if(diff.toString().length===3){
+                    n="0"+diff.toString();
+                }
+                else if(diff.toString().length===2){
+                    n="00"+diff.toString();
+                }
+                else{
+                    n="000"+diff.toString();
+                }
+    
+                console.log("Next n=", n);
+            }  
+        }
+        console.log("Stop");
+    }
+    else{
+        console.log("Invalid");
+    }
+}
+
+
+var num = Math.floor(Math.random() * 9901) + 100;  // Math.floor(Math.random() * (max - min + 1) ) + min; // Here max = 10000, min = 100
+Kaprekar_routine(num);
