e0b533ea3e74455b616dfdb249fd752f02bc3077
Sampad Mandal     ................................................           সম্পদ মণ্ডল <sampadmandals@gmail.com>
2025-03-18 07:59:49 +0530

    Add files via upload

--- value_swap_without_third_variable.js
+++ value_swap_without_third_variable.js
@@ -2,18 +2,37 @@
 let a=30;
 let b=20;
 
-b = b+a; //30+20=50
+b = b+a; //20+30=50
 a = b-a; //50-30=20
 b = b-a; //50-20=30
 */
 
-let a=20;
-let b=30;
+// let a=20;
+// let b=30;
 
-a = a+b; //20+30=50
-b = a-b; //50-30=20
-a = a-b; //50-20=30
+// a = a+b; //20+30=50
+// b = a-b; //50-30=20
+// a = a-b; //50-20=30
 
 
-console.log(`a=${a}`);
-console.log(`b=${b}`);
+// console.log(`a=${a}`);
+// console.log(`b=${b}`);
+
+
+let a = -9;
+let b = 11;
+
+if(a>b){
+    b = b+a;
+    a = b-a;
+    b = b-a;
+    console.log(`a=${a}`);
+    console.log(`b=${b}`);
+}
+else{
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    console.log(`a=${a}`);
+    console.log(`b=${b}`);
+}
