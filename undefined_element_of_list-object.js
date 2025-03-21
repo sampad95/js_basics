44eca783ae444adc4eb5bb22e00e22ed57907aa1
sampad95 <sampadmandals@gmail.com>
2025-03-21 23:25:12 +0530

    added

--- undefined_element_of_list-object.js
+++ undefined_element_of_list-object.js
@@ -5,3 +5,11 @@ console.log(lst.length);
 console.log(lst);
 console.log(lst[3]);
 console.log(typeof lst);
+
+let lst2 = [1, 2, 3, 4];
+delete lst2[2];
+
+console.log(lst2.length);
+console.log(lst2);
+console.log(lst2[2]);
+console.log(typeof lst2);
