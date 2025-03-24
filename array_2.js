90319eda8410686fd2901802196b06ee8813e385
sampad95 <sampadmandals@gmail.com>
2025-03-24 19:21:15 +0530

    added

---
+++ array_2.js
@@ -0,0 +1,15 @@
+let a = [] + [];
+let b = [1] + [];
+let c = [1] + "abc";
+let d = [1, 2] + [3, 4];
+
+
+console.log(a);
+console.log(typeof a);
+console.log(b);
+console.log(typeof b);
+console.log(c);
+console.log(typeof c);
+console.log(d);
+console.log(typeof d);
+
